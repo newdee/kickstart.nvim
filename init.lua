7dba73b905eb217dbb6e5d740eace5547a5249c9
dfine <dfine@nnkosmos.com>
2024-05-13 16:46:16 +0800

    feat: add come plugin

--- init.lua
+++ init.lua
@@ -654,6 +654,8 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
+        json = { 'jq' },
+        go = { 'goimports', 'gofmt' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
