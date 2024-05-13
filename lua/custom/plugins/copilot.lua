f0648459ee7980b7e50c98e8add48cc25d4b7f81
dfine <dfine@nnkosmos.com>
2024-05-13 20:54:35 +0800

    feat: modify  nvim plugin

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,5 +1,16 @@
 return {
-  {
-    'github/copilot.vim',
+  'github/copilot.vim',
+  event = {
+    'CmdlineEnter',
+    'InsertEnter',
   },
+  config = function()
+    if os.getenv 'copilot_proxy' ~= nil then
+      local proxy_url = os.getenv 'copilot_proxy' --[[@as string]]
+      proxy_url = string.gsub(proxy_url, '^[^:]+://', '')
+      proxy_url = string.gsub(proxy_url, '/$', '')
+
+      vim.g.copilot_proxy = proxy_url
+    end
+  end,
 }
