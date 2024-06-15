ce6d0d6e0902651e7ff696ec2f0da17423d5f06a
dfine <dfine@nnkosmos.com>
2024-06-15 14:55:10 +0800

    feat: add bookmark to browser

--- lua/custom/plugins/browser.lua
+++ lua/custom/plugins/browser.lua
@@ -10,21 +10,28 @@ return {
       -- either pass it here or just pass the table to the functions
       -- see below for more
       bookmarks = {
-        ['Github'] = {
-          ['name'] = 'Github Search',
-          ['code_search'] = 'https://github.com/search?q=%s&type=code',
-          ['repo_search'] = 'https://github.com/search?q=%s&type=repositories',
+        ['Search'] = {
+          ['name'] = 'Search',
+          ['Code'] = 'https://github.com/search?q=%s&type=code',
+          ['Repo'] = 'https://github.com/search?q=%s&type=repositories',
+          ['Reddit'] = 'https://www.reddit.com/search/?q=%s',
+          ['Bing'] = 'https://www.bing.com/search?q=%s',
+          ['Duckduckgo'] = 'https://duckduckgo.com/?q=%s',
         },
         ['Work'] = {
-          ['name'] = 'Work Search',
+          ['name'] = 'Work Web',
           ['ChatGpt'] = 'https://chatgpt.com',
           ['BiliBili'] = 'https://www.bilibili.com',
+          ['Gmail'] = 'https://mail.google.com/mail/u/0/#inbox',
+          ['Trending'] = 'https://github.com/trending',
+          ['Reddit'] = 'https://www.reddit.com/r/programming/',
+          ['Neovim'] = 'https://neovim.io',
         },
       },
       icons = {
-        bookmark_alias = '', -- if you have nerd fonts, you can set this to ""
-        bookmarks_prompt = '󰂺 ', -- if you have nerd fonts, you can set this to "󰂺 "
-        grouped_bookmarks = '', -- if you have nerd fonts, you can set this to 
+        bookmark_alias = '󱍻', -- if you have nerd fonts, you can set this to ""
+        bookmarks_prompt = '󱖮', -- if you have nerd fonts, you can set this to "󰂺 "
+        grouped_bookmarks = '', -- if you have nerd fonts, you can set this to 
       },
       vim.keymap.set('n', '<leader>ob', '<cmd>lua require("browse").open_bookmarks()<cr>', { desc = 'Open browser Bookmark' }),
       vim.keymap.set('n', '<leader>sc', '<cmd>lua require("browse").input_search()<cr>', { desc = 'Search Context with browser' }),
