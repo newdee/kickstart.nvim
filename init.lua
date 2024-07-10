1526055ff0c02cdf4026474be4016821f87117f9
dfine <dfine@nnkosmos.com>
2024-07-10 17:40:59 +0800

    feat: add iron && rm rustocean

--- init.lua
+++ init.lua
@@ -570,7 +570,7 @@ require('lazy').setup({
         ruff = {},
         basedpyright = {},
         actionlint = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -2,3 +2,4 @@ return {
   'mfussenegger/nvim-dap',
   lazy = false,
 }
+-- return { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
